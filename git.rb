2abc7e5f3f548379cc6e729c4513fda410ac7cfe
AJ Harmond <ajharmond@gmail.com>
2019-02-14 11:11:21 -0700

    display git config

--- git.rb
+++ git.rb
@@ -1,5 +1,15 @@
 module Git
+<<<<<<< HEAD
   def self.git_cmd(cmd)
     puts `git #{cmd} -h`
   end
+=======
+  def self.display_cmd(cmd)
+    puts `git #{cmd} -h`
+  end
+
+  def self.config
+    puts `git config -l`
+  end
+>>>>>>> display git config
 end
--- main.rb
+++ main.rb
@@ -1,27 +1,39 @@
 require 'colorize'
+<<<<<<< HEAD
 require_relative 'git.rb'
+=======
+require_relative 'git'
+>>>>>>> display git config
 
 def puts_git(cmd)
   puts `git #{cmd} -h`
   menu
 end
 
-def menu
+def self.menu
   puts "MAIN MENU".colorize(:cyan)
   puts "1: Enter git command".colorize(:cyan)
-  puts "2: Exit".colorize(:cyan)
+  puts "2: View git config".colorize(:cyan)
+  puts "3: Exit".colorize(:cyan)
   choice = gets.to_i
   case choice
   when 1
     puts "Enter git command".colorize(:green)
+<<<<<<< HEAD
     Git.git_cmd(gets.strip)
+=======
+    Git.display_cmd(gets.strip)
+>>>>>>> display git config
     menu
   when 2
+    Git.config
+  when 3
     exit
   else
     puts "Invalid Choice".colorize(:red)
-    menu
   end
+
+  menu
 end
 
 menu
